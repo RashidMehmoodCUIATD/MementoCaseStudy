fe89814e0e73a4d5da52c457ae4aa9f9aabdabff
Rashid Mehmood <s.rashidmehmood@outlook.com>
2022-12-22 08:35:19 +0500

    initial

---
+++ src/main/java/com/example/demofx/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.demofx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onUndoClick() {
+
+        //perform State saving Actions
+    }
+    @FXML
+    protected void RequiestSaveState() {
+
+        //perform State saving Actions
+    }
+    @FXML
+    protected void redoClicked() {
+
+        //perform State saving Actions
+    }
+}
